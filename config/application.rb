fd0a7c8df27a81a2af7bbd79aed5a0ce2fdf4dab
RikutoYamada <88074385+RikutoYamada@users.noreply.github.com>
2023-03-06 21:48:39 +0900

    refactoring

--- config/application.rb
+++ config/application.rb
@@ -22,9 +22,6 @@ module TamemoApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    config.middleware.use ActionDispatch::Cookies
-
-
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -37,5 +34,6 @@ module TamemoApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.middleware.use ActionDispatch::Cookies
   end
 end
