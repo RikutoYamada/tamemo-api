347d3e14af1cd73b3351f22262a65cfa8a0cdfb1
RikutoYamada <88074385+RikutoYamada@users.noreply.github.com>
2023-03-06 22:09:29 +0900

    Add api dir

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  post '/sign_in', to: 'sessions#create'
+  namespace 'api' do
+    namespace 'v1' do
+      post '/sign_in', to: 'sessions#create'
+    end
+  end
 end
