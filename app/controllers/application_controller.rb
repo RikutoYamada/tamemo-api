e79f65740fd367addd383572f3ee7f3b398342c5
RikutoYamada <88074385+RikutoYamada@users.noreply.github.com>
2023-04-19 20:57:25 +0900

    Add jwt-related functions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::API
-  include ActionController::Cookies
+  include JsonWebToken
+
+  def not_found
+    render json: { error: 'not_found' }
+  end
+
+  def authorize_request
+    token = request.cookies['token']
+    begin
+      decoded = jwt_decode(token)
+      logger.debug(decoded)
+      # @current_user = User.find()
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { errors: e.message }, status: :unauthorized
+    rescue JWT::DecodeError => e
+      render json: { errors: e.message }, status: :unauthorized
+    end
+  end
 end
