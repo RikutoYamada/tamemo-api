d0f8338cb834b3fc2e0cda02157a61a14f02f667
RikutoYamada <88074385+RikutoYamada@users.noreply.github.com>
2023-04-24 22:09:43 +0900

    Add user create action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::UsersController < ApplicationController
-  before_action :authorize_request
+  include ActionController::Cookies
+  before_action :authorize_request, except: [:create]
   def index
     @users = User.all
     render json: @users, status: :ok
@@ -8,5 +9,17 @@ class Api::V1::UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     render json: @user
+  end
+
+  def create
+    @user = User.create(name: params[:name], email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation])
+    token = jwt_encode(params[:email])
+
+    cookies[:token] = {
+      value: token,
+      httponly: true
+    }
+
+    render json: @user, status: :created
   end
 end
