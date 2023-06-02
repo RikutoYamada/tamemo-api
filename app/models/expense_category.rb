e58f0a15d2545cd7089a533174c506f4a06cbe40
RikutoYamada <88074385+RikutoYamada@users.noreply.github.com>
2023-06-02 22:49:23 +0900

    Remove expense_categories table

--- app/models/expense_category.rb
+++
@@ -1,5 +0,0 @@
-class ExpenseCategory < ApplicationRecord
-  belongs_to :user
-  has_many :expenses, dependent: :destroy
-  has_ancestry
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_27_123040) do
-  create_table "expense_categories", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name"
-    t.string "ancestry"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "user_id"
-    t.index ["user_id"], name: "index_expense_categories_on_user_id"
-  end
-
+ActiveRecord::Schema[7.0].define(version: 2023_06_02_134554) do
   create_table "expenses", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "store"
     t.integer "amount"
@@ -49,7 +40,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_27_123040) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "expense_categories", "users"
   add_foreign_key "expenses", "users"
   add_foreign_key "sub_expense_categories", "users"
 end
