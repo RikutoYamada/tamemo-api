e0a3212c97fc7e93467f2f212b17f3e114894c49
RikutoYamada <rikutoyamada1992@gmail.com>
2023-08-15 22:18:29 +0900

    Changed to allow control over the number of records returned as a response

--- app/controllers/api/v1/expenses_controller.rb
+++ app/controllers/api/v1/expenses_controller.rb
@@ -1,9 +1,15 @@
 class Api::V1::ExpensesController < ApplicationController
   def index
-    expenses = Expense.includes(:sub_expense_category).where(user_id: params[:user_id])
+    max_records = params[:max_records] || 5
+    expenses = Expense.includes(:sub_expense_category)
+                      .where(user_id: params[:user_id])
+                      .order(expended_at: :desc)
+                      .limit(max_records)
+
     expenses_with_category = expenses.map do |expense|
       sub_category = expense.sub_expense_category
       main_category = sub_category.main_expense_category
+
       {
         id: expense.id,
         amount: expense.amount,
